b70700812c71875afd902ba2ae3e576939bfb17b
ceec <administrador.app@ceec.com.pe>
2025-08-07 15:23:57 -0500

    updated-datos

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
-  'https://copilot-rust.vercel.app/',
+  'https://copilot-rust.vercel.app',
 ];
 
 app.use(
